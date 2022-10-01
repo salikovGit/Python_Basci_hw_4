2783bc76441004dfbafc5f6d22b78269fd92276c
salikovGit <iesalikov@gmail.com>
2022-10-02 00:42:26 +0500

    task 3

---
+++ hw_4_3.py
@@ -0,0 +1,35 @@
+'''
+3 - В файле, содержащем фамилии студентов и их оценки, изменить на прописные буквы фамилии тех студентов,
+которые имеют средний балл более «4».
+Пример:
+Волков Андрей 5
+Наталья Тарасова 5
+Фредди Меркури 3
+Денис Байцуров 4
+
+Программа выдаст:
+ВОЛКОВ АНДРЕЙ 5
+НАТАЛЬЯ ТАРАСОВА 5
+Фредди Меркури 3
+Денис Байцуров 4
+'''
+
+
+def upper_case(file_name):
+    text = ''
+    with open(file_name, 'r', encoding='utf-8') as file:
+        for line in file.readlines():
+            a = line.split()
+            if int(a[-1]) == 5:
+                a = ' '.join(a)
+                text = text + a.upper() + '\n'
+                print(text)
+            else:
+                a = ' '.join(a)
+                text = text + a + '\n'
+                print(text)
+    with open(file_name, 'w', encoding='utf-8') as file:
+        file.write(text)
+
+
+upper_case('names.txt')
